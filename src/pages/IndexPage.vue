5eca4bef0579c9282d56a7860b9d5e04b7726798
mateuszmistecki <mateusz.mistecki@gmail.com>
2023-02-27 21:54:29 +0100

    Add PlaySound page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <q-btn to="PlaySound" label="PlaySound" color="primary"></q-btn>
   </q-page>
 </template>
 
