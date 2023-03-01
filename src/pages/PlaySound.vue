a7c0ad1f3e07f3f3b1d70e0563e8859c8ff3764f
mateuszmistecki <mateusz.mistecki@gmail.com>
2023-03-01 17:39:15 +0100

    Add gutter and margins

--- src/pages/PlaySound.vue
+++ src/pages/PlaySound.vue
@@ -1,19 +1,34 @@
 <template>
   <q-page padding>
-    <q-btn
-      v-for="item in syllables"
-      v-bind:key="item"
-      style="width: 150px; height: 80px"
-    >
-      <div>
-        <span class="text-h3">{{ item }}</span>
-      </div>
-    </q-btn>
+    <div class="q-gutter-md fit row wrap q-mx-auto q-my-auto">
+      <q-btn
+        style="width: 150px; height: 80px"
+        v-for="item in syllables"
+        v-bind:key="item"
+      >
+        <div>
+          <span class="text-h3">{{ item }}</span>
+        </div>
+      </q-btn>
+    </div>
   </q-page>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
-const syllables = ref(['PA', 'PO', 'PE', 'PI', 'PU', 'PY'])
+const syllables = ref([
+  'PA',
+  'PO',
+  'PE',
+  'PI',
+  'PU',
+  'PY',
+  'PA',
+  'PO',
+  'PE',
+  'PI',
+  'PU',
+  'PY',
+])
 </script>
