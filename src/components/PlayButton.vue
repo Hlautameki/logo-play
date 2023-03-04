7cbaef18c0e6783c3ab5e3334dcae176e13a0bdd
mateuszmistecki <mateusz.mistecki@gmail.com>
2023-03-04 20:59:59 +0100

    Notification if there is no mp3 file after button click

--- src/components/PlayButton.vue
+++ src/components/PlayButton.vue
@@ -8,10 +8,15 @@
 
 <script setup>
 import { useSound } from '@vueuse/sound'
+import { useQuasar } from 'quasar'
+
+const $q = useQuasar()
 const props = defineProps({
   label: String,
 })
+
 let syllable
+
 try {
   syllable = useSound(require(`../assets/${props.label}.mp3`))
 } catch (e) {
@@ -21,6 +26,11 @@ try {
 function clicked() {
   if (syllable) {
     syllable.play()
+  } else {
+    $q.notify({
+      message: `There is no sound file for \"${props.label}\"`,
+      color: 'negative',
+    })
   }
 }
 </script>
