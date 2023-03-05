1084a34d2bc4cd371a8f1c2c5ac69563cba4a904
mateuszmistecki <mateusz.mistecki@gmail.com>
2023-03-05 13:56:40 +0100

    Add IndicatePage.

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,20 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex flex-center column q-gutter-md">
     <q-btn
       to="PlaySound"
-      label="Listen"
+      label="Słuchaj"
       color="primary"
       icon="volume_down"
+      size="15px"
+      style="width: 140px"
+    ></q-btn>
+    <q-btn
+      to="Indicate"
+      label="Wskaż"
+      color="primary"
+      icon="touch_app"
+      size="15px"
+      style="width: 140px"
     ></q-btn>
   </q-page>
 </template>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,20 +1,20 @@
-
 const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'playsound', component: () => import('pages/PlaySound.vue')}
-    ]
+      { path: 'playsound', component: () => import('pages/PlaySound.vue') },
+      { path: 'indicate', component: () => import('pages/IndicatePage.vue') },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
 ]
 
 export default routes
