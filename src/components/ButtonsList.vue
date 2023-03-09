75b8752f374fc4c78b8f4d1c33141c8c398dca60
mateuszmistecki <mateusz.mistecki@gmail.com>
2023-03-09 18:12:59 +0100

    Use flexbox grid model for row

--- src/components/ButtonsList.vue
+++ src/components/ButtonsList.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="q-gutter-md fit row wrap q-mx-auto q-my-auto">
-    <play-button
-      style="width: 150px; height: 80px"
-      v-for="item in syllables"
-      v-bind:key="item"
-      :label="item"
+  <!--  <div class="row q-col-gutter-x-md">-->
+  <!--    <play-button-->
+  <!--      style="height: 80px"-->
+  <!--      v-for="item in syllables"-->
+  <!--      v-bind:key="item"-->
+  <!--      :label="item"-->
+  <!--      class="col-2"-->
+  <!--    >-->
+  <!--    </play-button>-->
+  <!--  </div>-->
+  <div class="row q-col-gutter-md">
+    <div
+      class="col-4 col-md-2"
+      v-for="n in 6"
+      :key="`none-${n}`"
     >
-    </play-button>
+      <!--      <div style="background: #26a69a">&nbsp;</div>-->
+      <div
+        style="background: #26a69a"
+        class="row"
+      >
+        <q-btn
+          class="col-12"
+          label="PA"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import PlayButton from 'components/PlayButton.vue'
+
 const props = defineProps({
   syllables: [],
 })
 </script>
+
+<style></style>
