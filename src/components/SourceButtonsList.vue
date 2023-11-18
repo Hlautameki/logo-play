55b1b884ee63109f7669435be66cfc01187dc5ac
mateuszmistecki <mateusz.mistecki@gmail.com>
2023-11-18 17:11:26 +0100

    Change SourceButtonsList.vue height depending on buttons count.

--- src/components/SourceButtonsList.vue
+++ src/components/SourceButtonsList.vue
@@ -1,38 +1,47 @@
 <template>
-  <container
-    style="display: flex; touch-action: none"
-    class="row q-col-gutter-md"
-    orientation="horizontal"
-    :get-ghost-parent="getGhostParent"
-    :get-child-payload="getChildPayload"
-    @drop="onDrop"
-    @drop-ready="onDropReady"
-    @drop-not-allowed="dropNotAllowed"
-    @drag-enter="onDragEnter"
-    group-name="1"
-    @drag-start="onDragStart"
-    @drag-leave="onDragLeave"
-    :should-accept-drop="(src, payload) => getShouldAcceptDrop(1, src, payload)"
-    :drag-begin-delay="0"
+  <div
+    class="row"
+    :class="classObject"
   >
-    <draggable
-      style="display: flex; height: auto"
-      class="col-6 col-md-2 col-sm-4 row items-stretch"
-      v-for="item in syllables"
-      v-bind:key="item.syllable"
-      :drag-not-allowed="false"
-    >
-      <source-button
-        class="col-12"
-        :item="item"
-      />
-    </draggable>
-  </container>
+    <div class="col column justify-between">
+      <container
+        style="display: flex; touch-action: none"
+        class="row col q-col-gutter-md"
+        orientation="horizontal"
+        :get-ghost-parent="getGhostParent"
+        :get-child-payload="getChildPayload"
+        @drop="onDrop"
+        @drop-ready="onDropReady"
+        @drop-not-allowed="dropNotAllowed"
+        @drag-enter="onDragEnter"
+        group-name="1"
+        @drag-start="onDragStart"
+        @drag-leave="onDragLeave"
+        :should-accept-drop="
+          (src, payload) => getShouldAcceptDrop(1, src, payload)
+        "
+        :drag-begin-delay="0"
+      >
+        <draggable
+          style="display: flex; height: auto"
+          class="col-6 col-md-2 col-sm-4 row items-stretch"
+          v-for="item in syllables"
+          v-bind:key="item.syllable"
+          :drag-not-allowed="false"
+        >
+          <source-button
+            class="col-12"
+            :item="item"
+          />
+        </draggable>
+      </container>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import SourceButton from 'components/SourceButton.vue'
-import { defineEmits, ref } from 'vue'
+import { reactive, ref } from 'vue'
 import { Container, Draggable } from 'vue-dndrop'
 import { applyDrag } from '../utils/helpers'
 
@@ -48,6 +57,23 @@ const getShouldAcceptDrop = (index, sourceContainerOptions, payload) => {
 
 const onDrop = (dropResult) => {
   syllables.value = applyDrag(syllables.value, dropResult)
+  emit('changeMediaQueries', syllables.value?.length)
+
+  if (syllables.value?.length < 5) {
+    classObject['col-xs-6'] = false
+    classObject['col-xs-4'] = true
+  }
+
+  if (syllables.value?.length < 3) {
+    classObject['col-xs-4'] = false
+    classObject['col-xs-2'] = true
+  }
+
+  if (syllables.value?.length < 4) {
+    classObject['col-sm-4'] = false
+    classObject['col-sm-2'] = true
+  }
+  console.log(classObject)
 }
 
 const getGhostParent = () => {
@@ -72,7 +98,12 @@ const getChildPayload = (index) => {
   return syllables.value[index]
 }
 
-const handleCustomEvent = (arg) => emit('dropOnSourceEvent', arg)
-
-const emit = defineEmits(['dropOnSourceEvent'])
+const classObject = reactive({
+  'col-xs-6': true,
+  'col-md-2': true,
+  'col-sm-4': true,
+  'col-xs-4': false,
+  'col-xs-2': false,
+  'col-sm-2': false,
+})
 </script>
--- src/pages/SequencesPage.vue
+++ src/pages/SequencesPage.vue
@@ -15,11 +15,7 @@
           />
         </template>
       </buttons-list>
-      <source-buttons-list
-        class="col-6 col-md-2 col-sm-4"
-        :syllablesProp="sourceSyllables"
-        @dropOnSourceEvent="handleDropOnSourceEvent"
-      />
+      <source-buttons-list :syllablesProp="sourceSyllables" />
     </div>
   </q-page>
 </template>
@@ -28,7 +24,7 @@
 import { useRoute } from 'vue-router'
 import { useSyllablesWithPathBuilder } from 'src/reusable/syllablesWithPathBuilder'
 import SourceButtonsList from 'components/SourceButtonsList.vue'
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 import ButtonsList from 'components/ButtonsList.vue'
 import DestinationButton from 'components/DestinationButton.vue'
 
@@ -38,11 +34,4 @@ const { getSyllablesWithPath } = useSyllablesWithPathBuilder()
 const syllables = ref(getSyllablesWithPath(consonant))
 let sourceSyllables = [...syllables.value]
 sourceSyllables = sourceSyllables.sort((a, b) => 0.5 - Math.random())
-const handleDropOnSourceEvent = (arg) => {
-  syllables.value.forEach((syll) => {
-    if (syll.syllable === arg) {
-      syll.matched = true
-    }
-  })
-}
 </script>
