0db97f5d36c2db32593f86d5ae1b3b10488a0e4b
mateuszmistecki <mateusz.mistecki@gmail.com>
2024-02-20 21:14:51 +0100

    Index page buttons are big responsive squares

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,36 +1,54 @@
 <template>
-  <q-page class="flex flex-center column q-gutter-md">
-    <q-btn
-      to="listen"
-      label="Słuchaj"
-      color="primary"
-      icon="volume_down"
-      size="15px"
-      style="width: 160px"
-    ></q-btn>
-    <q-btn
-      to="Indicate"
-      label="Wskaż"
-      color="primary"
-      icon="touch_app"
-      size="15px"
-      style="width: 160px"
-    ></q-btn>
-    <q-btn
-      to="Sequences"
-      label="Sekwencje"
-      color="primary"
-      icon="swipe_right_alt"
-      size="15px"
-      style="width: 160px"
-    ></q-btn>
+  <q-page class="q-pa-md q-col-gutter-lg row content-start">
+    <div
+      :class="classArray"
+      class="row"
+    >
+      <q-btn
+        style="aspect-ratio: 1; min-width: 202px"
+        class="col"
+        to="listen"
+        label="Słuchaj"
+        color="primary"
+        icon="volume_down"
+        size="lg"
+      />
+    </div>
+    <div
+      :class="classArray"
+      class="row"
+    >
+      <q-btn
+        style="aspect-ratio: 1; min-width: 202px"
+        class="col"
+        to="Indicate"
+        label="Wskaż"
+        color="primary"
+        size="lg"
+        icon="touch_app"
+      />
+    </div>
+    <div
+      :class="classArray"
+      class="row"
+    >
+      <q-btn
+        style="aspect-ratio: 1; min-width: 202px"
+        class="col"
+        to="Sequences"
+        label="Sekwencje"
+        color="primary"
+        size="lg"
+        icon="swipe_right_alt"
+      />
+    </div>
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
+import { computed, ref } from 'vue'
 
-export default defineComponent({
-  name: 'IndexPage',
-})
+const classArray = ['col-xs-12', 'col-sm-4', 'col-md-2', 'col-lg-2', 'col-xl-2']
+const btnRef = ref(null)
+const btnHeight = computed(() => 300)
 </script>
